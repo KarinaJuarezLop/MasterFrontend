96ce61f04cb6ec823f70a55e8dc1fd209cb10312
Karina Juarez <a356313@alumnos.uaslp.mx>
2025-02-12 16:46:10 -0600

    Actividad de la leccion 1

---
+++ tipos-de-datos.js
@@ -0,0 +1,17 @@
+// Ejercicios
+console.log(typeof 42);          // numero
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666);         // numero
+console.log(typeof true);         // boleano
+console.log(typeof 0);            // numero
+console.log(typeof "");           // string
+console.log(typeof null);         // object
+console.log(typeof undefined);    // undefined
+console.log(typeof FALSE);        // undefined (FALSE no está definido, debería ser false en minúsculas)
+
+// Ejemplos adicionales
+console.log(typeof 3.14);         // numero
+console.log(typeof 'Hola Mundo'); // string
+console.log(typeof {}); //object
+console.log(typeof false); //boleano
+console.log(typeof Infinity); //numero
