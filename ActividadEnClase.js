dce82c35fb0377b9ad01272aec0cf33aa3552341
Karina Juarez <a356313@alumnos.uaslp.mx>
2025-02-12 17:12:52 -0600

    corregi actividad de la leccion 1

---
+++ ActividadEnClase.js
@@ -0,0 +1,13 @@
+// Se le solicitan datos al cliente
+var nombre = prompt('Ingress tu nombre')
+var edad = prompt('Ingress tu edad')
+var gusto1 = prompt('Ingress tu pasatiempo favorito')
+var gusto2 = prompt('Ingress otro pasatiempo favorito')
+
+console.log(`Hola mi nombre es ${nombre} tengo la edad de ${edad} y me gusta ${gusto1} y tambien ${gusto2}`);
+
+var numero1 = prompt('ingresa el primer dato')
+var numero2 = prompt('ingresa el segundo dato')
+var numero3 = prompt('ingresa el tercer dato')
+
+console.log(Number(numero1) + Number(numero2) + Number(numero3));
--- tipos-de-datos.js
+++ tipos-de-datos.js
@@ -16,29 +16,3 @@ console.log(typeof {}); //object
 console.log(typeof false); //boleano
 console.log(typeof Infinity); //numero
 console.log(typeof infinity); //undefined
-
-//Ejercicio en clase
-//App para validar entrada al parque tematico
-
-//1. edad y altura, tener 12 años y medir mas de 140 cm
-//2. pago o pase, debe haber hevho el pago de $20 o tener un pase VIP
-//3. condicion de salud, No debe tener restricciones medicas
-//4. acompañante, si el visitante es menor de 16 años debe estar acompañado de un adulto
-
-let edad = Number(prompt('ingresa tu edad'))
-let altura = Number(prompt('ingresa tu altura en cm'))
-let pago = prompt('Hiciste el pago de la entrada? si - no'). toLocaleLowerCase()
-let pase = prompt('tienes un pase VIP? si - no'). toLocaleLowerCase()
-let salud = prompt('tienes alguna condicion medica? si - no'). toLocaleLowerCase()
-
-if(edad >= 12 && altura >= 140){
-    if(edad < 16){
-        let acompañante = prompt('Eres menor de 16, te acompaña un adulto? si - no'). toLocaleLowerCase()
-        if(acompañante === 'si'){
-            console.log('puedes ingresar');
-        }
-        else{
-            console.log('ya que eres menor y no te acompana un adulto, no puedes ingresar');
-        }
-        }
-} 
